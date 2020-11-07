17a50b1fa0187e3f6afc52c26ec2dc5052d5a777
DipinJassal <71757022+DipinJassal@users.noreply.github.com>
2020-11-07 21:59:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,17 @@ function removeTransition(e) {
     if(e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
+
+function clickTransition(e) {
+    const audio = document.querySelector(`audio[data-key="${this.getAttribute('data-key')}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    this.classList.add('playing');
+}
+
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+keys.forEach(key => key.addEventListener('click',clickTransition));
 
 window.addEventListener('keydown',playSound);
