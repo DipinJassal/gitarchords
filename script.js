696640896738a7583870aa1fa90840c9e4ea3393
DipinJassal <71757022+DipinJassal@users.noreply.github.com>
2020-11-07 12:27:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
+     if (e.repeat) return; 
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');   
